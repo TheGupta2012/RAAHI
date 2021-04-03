b95e1e81888a8d37fbcd311677cd85801e0bff08
deepakdpy <59403101+deepakdpy@users.noreply.github.com>
2021-04-03 16:27:24 +0530

    Added the speech output file

---
+++ openCV Lanes/Curved Lanes/tts.py
@@ -0,0 +1,19 @@
+import pyttsx3
+
+def speak(text, rate=0.6,voice_type="female",volume=1):
+    engine=pyttsx3.init()   
+    # speech rate
+    engine.setProperty('rate',rate*engine.getProperty('rate'))
+
+    # Voice
+    if(voice_type=="female"):
+        engine.setProperty('voice',engine.getProperty('voices')[1].id)
+    else:
+        engine.setProperty('voice',engine.getProperty('voices')[0].id)
+
+    # Volume)   
+    engine.setProperty('volume',volume) 
+    engine.say(text)
+    engine.runAndWait()
+
+
